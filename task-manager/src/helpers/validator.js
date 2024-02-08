f64bad3daeae1ece971301c1c9e86b64c50cb121
Narsapuram-Mamatha <nmamatha1808@gmail.com>
2024-02-08 06:31:33 +0530

    Added first assignment

---
+++ task-manager/src/helpers/validator.js
@@ -0,0 +1,53 @@
+
+class validator {
+    static validateTaskInfo(taskInfo) {
+        if (taskInfo.hasOwnProperty("id") &&
+            taskInfo.hasOwnProperty("title") &&
+            taskInfo.hasOwnProperty("description") &&
+            taskInfo.hasOwnProperty("completed") &&
+            this.isString(taskInfo.title) &&
+            this.isString(taskInfo.description)
+            && this.isBoolean(taskInfo.completed) &&
+            this.isNotEmpty(taskInfo.title) &&
+            this.isNotEmpty(taskInfo.description)
+        ) {
+            return {
+                "status": true,
+                "message": "task has been validated"
+            };
+        }
+        else {
+            return {
+                "status": false,
+                "message": "task info is malformed"
+            };
+        }
+    }
+
+    static vaildateTaskId(tasksData, userGivenTaskId) {
+        const tasks = tasksData.tasks;
+        let filteredtask = tasks.filter(task => task.id == userGivenTaskId);
+
+        if (filteredtask.length > 0) {
+            return {
+                "status": false,
+                "message": "Id already exists"
+            };
+        } else {
+            return {
+                "status": true,
+                "message": "task has valid id"
+            };
+        }
+    }
+    static isString(value) {
+        return typeof value === "string";
+    }
+    static isBoolean(value) {
+        return typeof value === "boolean";
+    }
+    static isNotEmpty(value) {
+        return value.length > 0;
+    }
+}
+module.exports = validator;
